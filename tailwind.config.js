7a1fcba473423b8e0ac72ddac0c28c2716d4ee16
Lee Junhyun <junhyun@Leeui-MacBookPro.local>
2023-07-31 15:37:36 +0900

    Upload template

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./posts/*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
